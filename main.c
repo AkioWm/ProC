0d39e48773475291e0505f91c8b8846320b5b094
Akio_Wm <cheikoss1@hotmail.fr>
2019-10-09 21:01:56 +0200

    Switch

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
-    printf("Hello world! \n");
-    return 0;
+   char rep;
+   printf("Entrer sur \"p\" pour jouer ou une autre touche pour arrêter le programme\n");
+   scanf("%s",&rep);
+
+   switch (rep)
+   {
+
+   case 112:
+      printf("Choix 1 \n");
+      break;
+   default:
+   printf("Au Revoir");
+   exit(0);
+
+    }
+
+
+   // printf("Enter name: ");
+   // scanf("%s", str1);
+   // printf("Entered Name: %s\n", str1);
+   // printf("Entered Website:%s", str2);
+
+   return(0);
 
 }
